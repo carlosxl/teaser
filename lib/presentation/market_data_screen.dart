607ed0ae79902f70d935d4164a54d34139465540
CarlosXL <carlosxl@gmail.com>
2018-09-15 18:37:46 +0800

    Add auto reload and random effect

--- lib/presentation/market_data_screen.dart
+++ lib/presentation/market_data_screen.dart
@@ -1,6 +1,10 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 
+import 'package:my_project/actions/actions.dart';
 import 'package:my_project/containers/market_data.dart';
 import 'package:my_project/localizations.dart';
 import 'package:my_project/models/app_store.dart';
@@ -13,7 +17,20 @@ class MarketDataScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Timer timer;
+    Random rg = Random();
+
     return StoreBuilder<AppState>(
+      onInit: (store) {
+        timer = Timer.periodic(Duration(milliseconds: 500), (timer) {
+          if (rg.nextDouble() < 0.3) {
+            store.dispatch(RefreshAction());
+          }
+        });
+      },
+      onDispose: (store) {
+        timer.cancel();
+      },
       builder: (context, store) {
         return Scaffold(
           appBar: AppBar(
